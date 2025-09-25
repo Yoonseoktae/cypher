a923acaba8409198e8ab234b7079edf597ce649e
ebridge-tomas <153583901+ebridge-tomas@users.noreply.github.com>
2025-09-25 14:57:20 +0900

    MVC 초기세팅

---
+++ app/Controllers/Api/AuthController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AuthController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
+
